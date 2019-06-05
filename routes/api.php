ff2b4c4ec6f0b2200ed6d3a81b292fcc046a9146
Matheus Baptista <matheus.baptista10@hotmail.com>
2019-06-05 08:24:52 -0300

    api auth implemented with passport

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,16 @@
 
 use Illuminate\Http\Request;
 
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', 'Auth\AuthApiController@login');
+    Route::group([
+        'middleware' => 'auth:api'
+      ], function() {
+          Route::get('logout', 'Auth\AuthApiController@logout');
+          Route::get('user', 'Auth\AuthApiController@user');
+      });
+});
+
 Route::apiResource('users', 'Api\UserController');
