dd73caec005dddd8eb458b8e852599e1e760a333
Adriano Eduardo <adrianoeduardo.rp@gmail.com>
2019-10-20 23:25:20 -0300

    Criação do userController

--- desafio/routes/api.php
+++ desafio/routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Http\Request;
 
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
-// Route::get('logout', 'AuthController@logout');
+Route::get('logout', 'AuthController@logout');
 // Route::get('user', 'AuthController@getAuthUser');
+
+Route::apiResource('users', 'UserController');
