20bc8cdb8da73479adeeb89607ccc5797e4c5a79
Adriano Eduardo <adrianoeduardo.rp@gmail.com>
2019-10-27 14:22:10 -0300

    instalação da dependencia de validação e criação das rotas

--- api/routes/api.php
+++ api/routes/api.php
@@ -13,6 +13,14 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('novo', 'AuthController@novo');
+  
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('usuarios', 'AuthController@usuarios');
+        Route::put('editar', 'AuthController@editar');
+        Route::delete('apagar', 'AuthController@apagar');
+    });
 });
