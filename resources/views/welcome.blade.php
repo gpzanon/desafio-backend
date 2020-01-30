0da5511faab3e266218671f697338ef63121bbbf
Nasser <nasserfarhatt@gmail.com>
2020-01-30 14:03:24 -0300

    Ajustes finais.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,7 @@
 
             .title {
                 font-size: 84px;
+                color: orange;
             }
 
             .links > a {
@@ -81,18 +82,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Desafio Backend - Seguralta
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/NsRF/desafio-backend">GitHub</a>
                 </div>
             </div>
         </div>
