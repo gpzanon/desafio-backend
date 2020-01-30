143b57e95d7d5f2dbe00d94152f22a9968a1203d
Nasser <nasserfarhatt@gmail.com>
2020-01-30 11:44:19 -0300

    Criação do Controller e definindo as rotas.

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+Route::get('/cadastro', 'CadastroController@index');
+Route::get('/cadastro/{id}', 'CadastroController@index', function($id)
+{
+    return $id;
+});
+
+Route::post('/cadastro/create', 'CadastroController@store');
+Route::post('/cadastro/edit/{id}', 'CadastroController@update', function($id)
+{
+    return 'ID '. $id.' editado';
+});
+
+Route::delete('/cadastro/delete/{id}', 'CadastroController@destroy', function($id)
+{
+    return 'ID '. $id.' deletado';
 });
